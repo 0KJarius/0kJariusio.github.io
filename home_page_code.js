47214a843d4db854164274efaeb4686412814a39
Jarius <keithjarius748@gmail.com>
2025-11-05 17:40:22 +0800

    New Landing page finished

---
+++ home_page_code.js
@@ -0,0 +1,11 @@
+document.getElementById("converCalc").addEventListener("click", function(){
+    location.href = "/conversion_calc/conver_calc.html";
+});
+
+document.getElementById("mathStuff").addEventListener("click", function(){
+    location.href = "/mathStuff/mathStuff.html";
+});
+
+document.getElementById("incomeTax").addEventListener("click", function(){
+    location.href = "/income_tax_calculator/income_tax.html";
+});
